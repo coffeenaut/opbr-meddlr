272ef78b073089921a3fc2989224c7982f6eafba
michael <michael.samiley@gmail.com>
2024-08-18 11:00:48 -0700

    fixed search ignoring first 4 medals and added qol changes on input

--- src/components/MedalList.jsx
+++ src/components/MedalList.jsx
@@ -1,5 +1,6 @@
 import AdjustmentIcon from '@heroicons/react/20/solid/AdjustmentsHorizontalIcon'
 import SearchIcon from '@heroicons/react/20/solid/MagnifyingGlassIcon'
+import ResetIcon from '@heroicons/react/20/solid/ArrowPathIcon'
 
 import Medal from '../components/Medal'
 import { isObjectEmpty, Timeout } from '../util/tools';
@@ -11,6 +12,7 @@ const MedalList = (props) => {
     const characterMedalLength = GetCharacterMedalLength()
     const [medals, setMedals] = useState([ ...Array(MedalData.length).keys() ])
     const searchInput = useRef(null)
+    const focusDivert = useRef(null)
     function filter(medalList) {
         //need to add character medal length to event medal to get the right index
         setMedals([...medalList.map(m => typeof m === "string" || m.type === String? parseInt(m.substring(1, m.length)) + characterMedalLength : m)])
@@ -26,7 +28,8 @@ const MedalList = (props) => {
         }
         const checkEnter = (e) => {
             if (e.keyCode === 13) {
-                console.log("enter!")
+                e.target.blur()
+
             }
         }
         const sortCategory = (a, b) => a.Category.localeCompare(b.Category)
@@ -38,7 +41,8 @@ const MedalList = (props) => {
             else
                 props.filterMedal(filter.medals)
         }
-        function filter(val) {
+        function filter(term) {
+            const val = term.toLowerCase()
             let nFilters = []
             filterList.forEach( c => {
                 nFilters = [...nFilters, ...c.sub.filter((s => s.Category.indexOf(val) > -1))]
@@ -48,7 +52,7 @@ const MedalList = (props) => {
     
         return (
             <>
-                <AdjustmentIcon onClick={showFilters} className='icon-medium-grey cursor-pointer' />
+                <AdjustmentIcon onClick={showFilters} className='icon-medium-grey cursor-pointer' title="Filter Medals" />
                 <div className={`flex flex-col rounded-md absolute filter-drop cursor-pointer max-h-60 overflow-y-auto gap-y-2 ${showFilter ? 'showdrop' : ''}`}>
                     <div>
                         <input className="w-full rounded-lg form-input"
@@ -86,7 +90,7 @@ const MedalList = (props) => {
         }
     }
     function resetMedals() {
-        setMedals([ ...Array(MedalData.length).keys() ].map( i => i+4))
+        setMedals([ ...Array(MedalData.length).keys() ].map( i => i))
     }
     function showMedal(index) {
         return medals.indexOf(index) > -1
@@ -96,9 +100,7 @@ const MedalList = (props) => {
         let result = []
         if(searchTerm) {
             result = SearchMedal(searchTerm)
-        }
-        else {
-            console.log("nothing")
+            console.log(result)
         }
         if(result.length > 0) {
             setMedals([...result])
@@ -106,12 +108,19 @@ const MedalList = (props) => {
         else {
             resetMedals()
         }
+        //focus off search input
+        searchInput.current.blur()
     }
     return (
         <>
         <div className='flex flex-col'>
+            <div></div>
             <div className='flex -translate-y-2.5 p-2 fixed justify-between medal-toolbar'>
-                <div className='flex w-3/4 lg:w-1/2 search'><input ref={searchInput} onKeyDown={handleKeyPress} className="w-full rounded-lg form-input" type="text" placeholder='search medals'></input><SearchIcon className='-translate-x-9 translate-y-1 icon-medium-grey cursor-pointer' onClick={search}/></div>
+                <div className='flex w-3/4 lg:w-1/2 search'>
+                    <input ref={searchInput} onKeyDown={handleKeyPress} className="w-full rounded-lg form-input" type="text" placeholder='search medals'></input>
+                    <SearchIcon className='-translate-x-9 translate-y-1 icon-medium-grey cursor-pointer' onClick={search}/>
+                    <ResetIcon className="cursor-pointer icon-medium-tertiary" onClick={resetMedals} title="Reset"/>
+                </div>
                 <div className='filter'>
                     <Filters filterMedal={filter}/>
                 </div>
