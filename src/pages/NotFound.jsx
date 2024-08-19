cedbe9642307558f1b9f873161884f49326a710a
michael <michael.samiley@gmail.com>
2024-08-19 09:26:27 -0700

    updated image link to use published relative path

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = (props) => {
         <>
         <div className="flex flex-col justify-center items-center w-full gap-6">
             <div className="notfound-header">404</div>
-            <img src="/emptyChest.svg" />
+            <img src="/opbr-meddlr/emptyChest.svg" />
             <div className="flex w-3/4 lg:w-1/2 notfound-text">{ props.message !== undefined ? props.message : "The page you are looking for is not here."}</div>
         </div>
         </>
